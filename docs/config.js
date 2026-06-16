4d5ea5f84dcff49a46ec74cb6f5f55e2cc5b20a0
LamHoaiLinh <lamhoailinh105@gmail.com>
2026-06-16 14:47:57 +0700

    config.js

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 // Sau khi deploy backend lên Render, thay URL này bằng link API của anh.
 // Ví dụ: window.NEWS_RADAR_API_BASE = "https://news-radar-pro-api.onrender.com";
-window.NEWS_RADAR_API_BASE = "http://localhost:8000";
+window.NEWS_RADAR_API_BASE = "https://doc-bao-lamhoailinh.onrender.com";
